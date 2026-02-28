16baff48d430c06773bfc48aff58912c82a1f0b3
rrueda-s <samuel.rueda364@pascualbravo.edu.co>
2026-02-27 22:19:18 -0500

    Se agrega el proyecto

---
+++ src/Carta.java
@@ -0,0 +1,56 @@
+import java.util.Random;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class Carta {
+
+    private int indice;
+
+    public Carta(Random r) {
+        this.indice = r.nextInt(52) + 1;
+    }
+
+    public int getIndice() {
+        return this.indice;
+    }
+
+    public void mostrar(int x, int y, JPanel pnl) {
+        JLabel lbl = new JLabel();
+        String nombreArchivo = "imagenes/carta" + this.indice + ".jpg";
+        ImageIcon imagen = new ImageIcon(getClass().getResource(nombreArchivo));
+        lbl.setIcon(imagen);
+        lbl.setBounds(x, y, imagen.getIconWidth(), imagen.getIconHeight());
+        pnl.add(lbl);
+
+        lbl.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent evt) {
+                JOptionPane.showMessageDialog(null,getNombre().toString() + " de " +getPinta().toString());
+            }
+        });
+    }
+
+    public Pinta getPinta() {
+        if (indice <= 13) {
+            return Pinta.TREBOL;
+        } else if (indice <= 26) {
+            return Pinta.PICA;
+        } else if (indice <= 39) {
+            return Pinta.CORAZON;
+        } else {
+            return Pinta.DIAMANTE;
+        }
+    }
+
+    public NombreCarta getNombre() {
+        int residuo = indice % 13;
+        if (residuo == 0)
+            residuo = 13;
+        return NombreCarta.values()[residuo - 1];
+    }
+
+}
---
+++ src/Jugador.java
@@ -0,0 +1,59 @@
+import java.util.Random;
+
+import javax.swing.JPanel;
+
+public class Jugador {
+
+    private final int TOTAL_CARTAS = 10;
+    private final int MARGEN_IZQUIERDA = 10;
+    private final int DISTANCIA_ENTRE_CARTAS = 50;
+    private final int MARGEN_SUPERIOR = 10;
+
+    private Random r = new Random();
+    private Carta[] cartas = new Carta[TOTAL_CARTAS];
+
+    public void repartir() {
+        for (int i = 0; i < TOTAL_CARTAS; i++) {
+            cartas[i] = new Carta(r);
+        }
+    }
+
+    public void mostrar(JPanel pnl) {
+        pnl.removeAll();
+        pnl.setLayout(null);
+        int posicion = MARGEN_IZQUIERDA + DISTANCIA_ENTRE_CARTAS * (TOTAL_CARTAS - 1);
+        for (Carta andrea : cartas) {
+            andrea.mostrar(posicion, MARGEN_SUPERIOR, pnl);
+            posicion -= DISTANCIA_ENTRE_CARTAS;
+        }
+        pnl.repaint();
+    }
+
+    public String getGrupos() {
+        String resultado = "No se encontraron grupos";
+
+        int[] contadores = new int[NombreCarta.values().length];
+        for (Carta carta : cartas) {
+            contadores[carta.getNombre().ordinal()]++;
+        }
+        boolean hayGrupos = false;
+        for (int i = 0; i < contadores.length; i++) {
+            if (contadores[i] >= 2) {
+                hayGrupos = true;
+                break;
+            }
+        }
+        if (hayGrupos) {
+            resultado = "Se encontraron los siguientes grupos:\n";
+            int indice = 0;
+            for (int contador : contadores) {
+                if (contador >= 2) {
+                    resultado += Grupo.values()[contador].toString() + " de " + NombreCarta.values()[indice].toString()
+                            + "\n";
+                }
+                indice++;
+            }
+        }
+        return resultado;
+    }
+}
